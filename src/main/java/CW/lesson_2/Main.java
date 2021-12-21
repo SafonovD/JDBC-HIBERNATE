b2bd3cb6e271e81f76bb0a525cb9a1af30e7e012
Dmitriy Safonov <d.lexed@gmail.com>
2021-12-21 22:45:58 +0300

    add practice: lesson 2 full

--- src/main/java/CW/lesson_2/Main.java
+++ src/main/java/CW/lesson_2/Main.java
@@ -1,9 +1,11 @@
 package CW.lesson_2;
 
 import CW.lesson_2.dao.CarDAO;
+import CW.lesson_2.dao.ClientDAO;
 import CW.lesson_2.dao.DAOFactory;
 import CW.lesson_2.dao.IDAOFactory;
 import CW.lesson_2.entity.Car;
+import CW.lesson_2.entity.Client;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +18,23 @@ public class Main {
     public static void main(String[] args) {
         IDAOFactory factory = DAOFactory.getInstance();
         CarDAO carDAO = factory.getCarDAO();
+        ClientDAO clientDAO = factory.getClientDAO();
+
+// ============== add(Clients) ============
+        Client client1 = new Client();
+        client1.setName("Петя");
+        client1.setAge(16);
+        client1.setPhone("+67577657");
+        clientDAO.add(client1);
 
 // ============== add(Car car) ============
 
-        Car car1 = new Car();
-        car1.setMark("Audi");
-        car1.setModel("A8");
-        car1.setPrice(15_000);
-
-        carDAO.add(car1);
+//        Car car1 = new Car();
+//        car1.setMark("Audi");
+//        car1.setModel("A8");
+//        car1.setPrice(15_000);
+//
+//        carDAO.add(car1);
 
 
 //        Car car2 = new Car();
@@ -35,6 +45,14 @@ public class Main {
 //        carDAO.add(car2);
 
 
+           // =========== getAll(Cliens) ============
+
+        List<Client> clients = clientDAO.getAll();
+        for (Client cl: clients ) {
+            System.out.println(cl);
+
+        }
+
 // =========== getAll() ============
 
 //        List<Car> cars = carDAO.getAll();
@@ -43,15 +61,26 @@ public class Main {
 //        }
 //
 
+        // =========== getById(int id Clients)  ============
+
+        Client client2 = clientDAO.getById(2);
+        System.out.println(client2);
+
 // =========== getById(int id)  ============
 
 //        Car car3 = carDAO.getById(2);
 //        System.out.println(car3);
 
 
-// ===========  updatePrice(int price, int carId) ============
+// ===========  updatePhone(String phone, int clientId) ============
+        clientDAO.updatePhone("+235453",1);
 
+// ===========  updatePrice(int price, int carId) ============
 //        carDAO.updatePrice(100_000, 1);
+
+// ===========  remove(String name) ============
+
+        clientDAO.remove("Петя");
 
 // ===========  remove(String mark) ============
 //        carDAO.remove("chevrolet");
--- src/main/java/CW/lesson_2/dao/ClientJDBCDao.java
+++ src/main/java/CW/lesson_2/dao/ClientJDBCDao.java
@@ -56,7 +56,6 @@ public class ClientJDBCDao implements ClientDAO {
                clients.add(client);
             }
 
-
         }catch (SQLException e){
             e.printStackTrace();
         }finally {
@@ -74,16 +73,97 @@ public class ClientJDBCDao implements ClientDAO {
 
     @Override
     public Client getById(int id) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        connection = getConnection();
+        try{
+            statement = connection.prepareStatement(" SELECT name,age,phone from clients where id = ?");
+            statement.setInt(1, id);
+
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()){
+                String name = rs.getString(1);
+                int age = rs.getInt(2);
+                String phone = rs.getString(3);
+                Client client = new Client(id,name,age,phone);
+                return client;
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            if(connection != null && statement != null){
+                try {
+                    connection.close();
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public void updatePhone(String phone, int clientId) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        connection = getConnection();
+        try{
+            statement = connection.prepareStatement( "UPDATE clients SET phone = ? WHERE id = ?");
+
+            statement.setString(1,phone);
+            statement.setInt(2,clientId);
+
+            int up = statement.executeUpdate();
+
+            System.out.println("обновлено записей " + up);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            if(connection != null && statement != null){
+                try {
+                    connection.close();
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
 
     }
 
     @Override
     public void remove(String name) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        connection = getConnection();
+
+        try{
+            statement = connection.prepareStatement("delete from clients where name = ?");
+
+            statement.setString(1,name);
+            int del = statement.executeUpdate();
+            System.out.println("Удалено записей "+ del);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            if(connection != null && statement != null){
+                try {
+                    connection.close();
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
